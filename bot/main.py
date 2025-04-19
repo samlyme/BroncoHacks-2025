3b555ebd57a486e5abc74fc31fe05f8bb1d1a05d
Sam Ly <sam@samly.me>
2025-04-18 21:24:54 -0700

    remove comments

--- bot/main.py
+++ bot/main.py
@@ -1,9 +1,11 @@
 import os
 import interactions
-from interactions import Client, Intents, listen, slash_command, SlashContext, OptionType, slash_option, Modal, ParagraphText, ShortText, User, Member, component_callback, modal_callback, ComponentContext, ComponentCommand, Button, ButtonStyle
+from interactions import Client, Intents, Permissions, listen, slash_command, SlashContext, OptionType, slash_default_member_permission, slash_option, Modal, ParagraphText, ShortText, User, Member, component_callback, modal_callback, ComponentContext, ComponentCommand, Button, ButtonStyle, integration_types
 from dotenv import load_dotenv
 
-bot = Client(intents=Intents.GUILDS | Intents.GUILD_MEMBERS | Intents.GUILD_MESSAGES)  # Create a bot instance with the specified intents
+# Create a bot instance with the specified intents
+bot = Client(intents=Intents.GUILDS |
+             Intents.GUILD_MEMBERS | Intents.GUILD_MESSAGES)
 # intents are what events we want to receive from discord, `DEFAULT` is usually fine
 
 
@@ -15,11 +17,8 @@ async def on_ready():
 
 
 @listen()
-async def on_member_join(event:interactions.api.events.discord.MemberAdd):
-    print(f"{event.member.username} joined") 
-
-@listen()
 async def on_member_join(event: interactions.api.events.discord.MemberAdd):
+    print(f"{event.member.username} joined")
 
     button = Button(
         style=ButtonStyle.PRIMARY,
@@ -36,16 +35,21 @@ async def on_member_join(event: interactions.api.events.discord.MemberAdd):
     except Exception as e:
         print(f"Couldn't DM {event.member.username}: {e}")
 
+
 @component_callback("open_join_form")
 async def on_button_click(ctx: ComponentContext):
     join_modal = Modal(
-        ShortText(label="Name", required=True, placeholder="Enter your name", custom_id="name_modal"),
-        ShortText(label="Email", required=True, placeholder="Enter your email", custom_id="email_modal"),
-        ShortText(label="Student ID", required=True, placeholder="Enter your student ID", custom_id="student_id_modal"),
+        ShortText(label="Name", required=True,
+                  placeholder="Enter your name", custom_id="name_modal"),
+        ShortText(label="Email", required=True,
+                  placeholder="Enter your email", custom_id="email_modal"),
+        ShortText(label="Student ID", required=True,
+                  placeholder="Enter your student ID", custom_id="student_id_modal"),
         title="Join Form",
         custom_id="join_form"
     )
     await ctx.send_modal(join_modal)
+
 
 @modal_callback("join_form")
 async def on_modal_submit(ctx: ComponentContext, name_modal: str, email_modal: str, student_id_modal: str):
@@ -53,43 +57,6 @@ async def on_modal_submit(ctx: ComponentContext, name_modal: str, email_modal: s
         f"✅ Thanks for submitting your info!\n**Name**: {name_modal}\n**Email**: {email_modal}\n**Student ID**: {student_id_modal}",
         ephemeral=True
     )
-
-
-
-
-## display modal when user joins the server
-# @listen()
-# async def on_member_join(event: interactions.api.events.discord.MemberAdd):
-#     button = interactions.Button(
-#         style=interactions.ButtonStyle.PRIMARY,
-#         label="Fill out join form",
-#         custom_id="open_join_form"
-#     )
-
-#     welcome_channel = await interactions.Channel.fetch("WELCOME_ID")
-#     await welcome_channel.send(
-#         f"Welcome {event.member.mention}! Please fill out the join form.",
-#         components=button
-#     )
-
-
-# @component_callback("open_join_form")
-# async def on_button_click(ctx: interactions.ComponentContext):
-#     join_modal = interactions.Modal(
-#         ShortText(label="Name", required=True, placeholder="Enter your name", custom_id="name_modal"),
-#         ShortText(label="Email", required=True, placeholder="Enter your email", custom_id="email_modal"),
-#         ShortText(label="Student ID", required=True, placeholder="Enter your student ID", custom_id="student_id_modal"),
-#         title="Join Form",
-#         custom_id="join_form"
-#     )
-#     await ctx.popup(join_modal)
-
-# @modal_callback("join_form")
-# async def on_modal_submit(ctx: interactions.ComponentContext, name_modal: str, email_modal: str, student_id_modal: str):
-#     await ctx.send(
-#         f"Thanks for submitting!\n**Name**: {name_modal}\n**Email**: {email_modal}\n**Student ID**: {student_id_modal}",
-#         ephemeral=True
-#     )
 
 
 @listen()
@@ -100,22 +67,22 @@ async def on_message_create(event: interactions.api.events.discord.MessageCreate
     if event.message.author.id != bot.user.id:
         await event.message.channel.send("lmao")
 
-## command to create a thread from the current channel
+# command to create a thread from the current channel
+
+
 @slash_command(name="chat", description="Create a thread from the current channel")
-##string option
+# string option
 @slash_option(
     name="string_option",
     description="A string option for the command",
     required=True,
     opt_type=OptionType.STRING
 )
-## function that takes a SlashContext and a string option
+# function that takes a SlashContext and a string option
 async def create_thread_with_option(ctx: SlashContext, string_option: str):
-    thread = await ctx.channel.create_thread(name=string_option, auto_archive_duration=60)
+    thread = await ctx.channel.create_thread(name=string_option,  # type: ignore
+                                             auto_archive_duration=60)  # type: ignore
     await ctx.send(f"Thread created: {thread.mention}")
-
-
-
 
 
 load_dotenv()
