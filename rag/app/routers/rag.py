88babb89aef0e90146733c2e02915cb89a013bac
Sam Ly <sam@samly.me>
2025-04-22 22:33:25 -0700

    clean

--- rag/app/routers/rag.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter, HTTPException
-from sqlmodel import select
-from app.db import SessionDep
-from app.models import Resource, ResourceCreate, ResourceUpdate
-
-router = APIRouter(prefix="/rag", tags=["Rag"])
